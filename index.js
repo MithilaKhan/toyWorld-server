83e12d5d387ea67b7e944e93d4976360f611fce0
MithilaKhan <mithilakhan082@gmail.com>
2023-05-20 04:48:05 +0600

    find single data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // middleware 
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    await client.connect();
     const database = client.db("ToyWorld");
     const toyCollection = database.collection("toyCollection");
 
@@ -61,6 +61,15 @@ async function run() {
         })
         .toArray();
         res.send(result)
+    })
+
+
+    // get data using id 
+    app.get("/allToy/:id" , async(req , res)=>{
+      const id =req.params.id 
+      const query = { _id :new ObjectId(id)};
+      const result = await toyCollection.findOne(query);
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
